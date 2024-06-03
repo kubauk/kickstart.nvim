89664118634784256cb34ace866e51e7bfc48c0a
Jakub Jamro <jakub.jamro@peachpayments.com>
2024-06-03 17:23:32 +0200

    adding config to custom config

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -23,6 +23,7 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
+    'microsoft/vscode-node-debug',
   },
   config = function()
     local dap = require 'dap'
@@ -42,6 +43,27 @@ return {
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
+        'vscode-js-debug',
+      },
+    }
+
+    dap.adapters['pwa-node'] = {
+      type = 'server',
+      host = 'localhost',
+      port = '${port}',
+      executable = {
+        command = 'node',
+        args = { '/opt/js-debug/src/dapDebugServer.js', '${port}' },
+      },
+    }
+
+    require('dap').configurations.javascript = {
+      {
+        type = 'pwa-node',
+        request = 'launch',
+        name = 'Launch file',
+        program = '${file}',
+        cwd = '${workspaceFolder}',
       },
     }
 
