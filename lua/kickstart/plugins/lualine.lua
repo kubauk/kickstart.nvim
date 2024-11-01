ed326f341a09d64e8e325d7a6c5657d0906ef754
Jakub Jamro <jakub.jamro@peachpayments.com>
2024-11-01 12:03:10 +0200

    Adding parent directory to filename
    As it's easier to see what handler is what handler

--- lua/kickstart/plugins/lualine.lua
+++ lua/kickstart/plugins/lualine.lua
@@ -4,6 +4,9 @@ return {
   config = function()
     require('lualine').setup {
       options = { theme = 'ayu_mirage' },
+      sections = {
+        lualine_c = { { 'filename', path = 4 } },
+      },
     }
   end,
 }
